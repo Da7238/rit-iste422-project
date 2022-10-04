4935348ae81a2023782530d0d72d714301b370db
Da7238 <da7238@rit.edu>
2022-10-03 20:23:26 -0400

    finished gradle file.

--- build/scripts/rit-iste422-project.bat
+++ build/scripts/rit-iste422-project.bat
@@ -67,7 +67,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\hamcrest-core-1.3.jar;%APP_HOME%\lib\log4j-core-2.13.3.jar;%APP_HOME%\lib\log4j-api-2.13.3.jar
+set CLASSPATH=%APP_HOME%\lib\rit-iste422-project-1.0-SNAPSHOT.jar
 
 
 @rem Execute rit-iste422-project
